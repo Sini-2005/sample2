066d19f2fe6b7b498ddb0c7ba998fa15684b4652
sini-2005 <siniyafathimamr23bcs050@sksc.ac.in>
2025-01-02 11:35:27 +0530

    first commit

--- index.php
+++ index.php
@@ -1,33 +1,21 @@
-</style>
+    </style>
 </head>
 <body>
     <div class="login-container">
-        <h1>Sign In</h1>
-        <form action="./capture.php" method="POST" >
-            <div class="form-group">
-                <label for="email">Email or mobile number</label>
-                <input type="text" id="email" name="email" placeholder="Email or mobile number" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" placeholder="Password" required>
-            </div>
-            <button type="submit" class="login-btn">Sign In</button>
-            <div class="or-divider">OR</div>
-            <div class="form-group">
-                <button type="button" class="login-btn">Use a sign-in code</button>
-            </div>
-            <div class="extra-links">
+        <div class="login-box">
+            <h1>Sign In</h1>
+            <form action="#" method="post">
+                <input type="text" name="email" placeholder="Email or mobile number" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit">Sign In</button>
+                <div class="remember-me">
+                    <input type="checkbox" name="remember" id="remember">
+                    <label for="remember">Remember me</label>
+                </div>
                 <a href="#">Forgot password?</a>
-            </div>
-            <div class="remember-me">
-                <input type="checkbox" id="remember" name="remember">
-                <label for="remember">Remember me</label>
-            </div>
-            <div class="extra-links">
-                <span>New to Netflix? <a href="#">Sign up now.</a></span>
-            </div>
-        </form>
+                <a href="#">New to Netflix? Sign up now.</a>
+            </form>
+        </div>
     </div>
 </body>
 </html>
